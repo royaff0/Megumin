d77d2097c3d92bfc5142e2826db52d9197045a43
roya <limxcy@gmail.com>
2017-06-18 17:56:36 +0800

    Use ExoPlayer

--- mobile/src/main/java/com/sqrtf/megumin/PlayerActivity.kt
+++ mobile/src/main/java/com/sqrtf/megumin/PlayerActivity.kt
@@ -1,30 +1,35 @@
 package com.sqrtf.megumin
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.text.TextUtils
 import android.util.Log
-import android.view.SurfaceView
 import android.view.View
-import android.widget.CheckBox
-import android.widget.SeekBar
-import android.widget.TextView
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelection
+import com.google.android.exoplayer2.trackselection.TrackSelector
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView
+import com.google.android.exoplayer2.upstream.BandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import com.sqrtf.common.activity.BaseActivity
 import com.sqrtf.common.api.ApiHelper
-import java.text.SimpleDateFormat
+
 
 class PlayerActivity : BaseActivity() {
-    private val mHideHandler = Handler()
+    val playerView: SimpleExoPlayerView by lazy { findViewById(R.id.fullscreen_content) as SimpleExoPlayerView }
 
-    @SuppressLint("SimpleDateFormat")
-    private val format = SimpleDateFormat("mm:ss")
-
-    private var mContentView: SurfaceView? = null
-    private val mHidePart2Runnable = Runnable {
-        mContentView!!.systemUiVisibility =
+    val mHidePart2Runnable = Runnable {
+        playerView.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LOW_PROFILE or
                         View.SYSTEM_UI_FLAG_FULLSCREEN or
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
@@ -33,24 +38,18 @@ class PlayerActivity : BaseActivity() {
                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
     }
 
-    private var mControlsView: View? = null
-    private val mShowPart2Runnable = Runnable {
+    val mShowPart2Runnable = Runnable {
         supportActionBar?.show()
-        mControlsView!!.visibility = View.VISIBLE
     }
 
-    private var progress: SeekBar? = null
-    private var lastTime: TextView? = null
 
-    private var draging = false
-    var breakPoint = -1
-    private var mVisible: Boolean = false
-    private val mHideRunnable = Runnable { hide() }
+    val mHideHandler = Handler()
+    var bandwidthMeter: BandwidthMeter = DefaultBandwidthMeter()
+    var videoTrackSelectionFactory: TrackSelection.Factory = AdaptiveTrackSelection.Factory(bandwidthMeter)
+    var trackSelector: TrackSelector = DefaultTrackSelector(videoTrackSelectionFactory)
+    val player: SimpleExoPlayer by lazy { ExoPlayerFactory.newSimpleInstance(this, trackSelector) }
 
-    var errorTextView: TextView? = null
-
-    var videoReady = false
-    var fixedUrl = ""
+    var lastPlayWhenReady = false
 
     companion object {
         fun intent(context: Context, url: String): Intent {
@@ -60,97 +59,72 @@ class PlayerActivity : BaseActivity() {
         }
 
         private val INTENT_KEY_URL = "INTENT_KEY_URL"
-
-        private val AUTO_HIDE_DELAY_MILLIS = 3000
-        private val AUTO_HIDE_DELAY_MILLIS_AFTER_DRAG = 5000
-
         private val UI_ANIMATION_DELAY = 100
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_player)
-
-        val url = intent.getStringExtra(INTENT_KEY_URL)
-        if (TextUtils.isEmpty(url)) {
-            throw IllegalArgumentException("Required url")
-        }
-
-        fixedUrl = ApiHelper.fixHttpUrl(url)
-        Log.i(this.localClassName, "playing:" + fixedUrl)
-
-        errorTextView = findViewById(R.id.error_message) as TextView
-        mControlsView = findViewById(R.id.fullscreen_content_controls)
-        mContentView = findViewById(R.id.fullscreen_content) as SurfaceView
-        progress = findViewById(R.id.progress) as SeekBar?
-        lastTime = findViewById(R.id.last_time) as TextView?
-        val playButton = findViewById(R.id.playButton) as CheckBox
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        mContentView!!.setOnClickListener { toggle() }
+        val url = intent.getStringExtra(INTENT_KEY_URL)
+        if (TextUtils.isEmpty(url)) throw IllegalArgumentException("Required url")
 
-        mVisible = true
+        val fixedUrl = ApiHelper.fixHttpUrl(url)
+        Log.i(this.localClassName, "playing:" + fixedUrl)
 
+        playerView.setControllerVisibilityListener {
+            if (it == View.VISIBLE) {
+                show()
+            } else {
+                hide()
+            }
+        }
+        playerView.player = player
+
+        val dataSourceFactory = DefaultDataSourceFactory(this, Util.getUserAgent(this, BuildConfig.APPLICATION_ID))
+        val extractorsFactory = DefaultExtractorsFactory()
+        val videoSource = ExtractorMediaSource(Uri.parse(fixedUrl), dataSourceFactory, extractorsFactory, null, null)
+        player.prepare(videoSource)
+
+        player.playWhenReady = true
+        lastPlayWhenReady = true
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
-        delayedHide(1000)
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        player.playWhenReady = lastPlayWhenReady
+    }
+
+    override fun onPause() {
+        super.onPause()
+        lastPlayWhenReady = player.playWhenReady
+        player.playWhenReady = false
     }
 
     override fun onDestroy() {
         super.onDestroy()
-    }
-
-    fun outputMessage(msg: String) {
-        errorTextView!!.text = errorTextView!!.text.toString() + msg + "\n"
-    }
-
-    fun clearMessage() {
-        errorTextView!!.text = null
-    }
-
-    private fun toggle() {
-        if (mVisible) {
-            hide()
-        } else {
-            show()
-        }
+        player.playWhenReady = false
+        player.release()
     }
 
     private fun hide() {
-        // Hide UI first
         supportActionBar?.hide()
-        mControlsView!!.visibility = View.GONE
-        mVisible = false
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
         mHideHandler.removeCallbacks(mShowPart2Runnable)
         mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY.toLong())
     }
 
     private fun show() {
-        // Show the system bar
-        mContentView!!.systemUiVisibility =
+        playerView.systemUiVisibility =
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-        mVisible = true
 
-        // Schedule a runnable to display UI elements after a delay
         mHideHandler.removeCallbacks(mHidePart2Runnable)
         mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY.toLong())
-
-        delayedHide(AUTO_HIDE_DELAY_MILLIS)
-    }
-
-    private fun disableAutoHide() {
-        mHideHandler.removeCallbacks(mHideRunnable)
-    }
-
-    private fun delayedHide(delayMillis: Int) {
-        mHideHandler.removeCallbacks(mHideRunnable)
-        mHideHandler.postDelayed(mHideRunnable, delayMillis.toLong())
     }
 }

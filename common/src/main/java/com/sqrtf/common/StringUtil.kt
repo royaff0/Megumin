3bcb56b65bb9ea579f1ea90fe9a473b56a9439be
roya <limxcy@gmail.com>
2017-08-25 14:02:20 +0800

    fix display issues

--- common/src/main/java/com/sqrtf/common/StringUtil.kt
+++ common/src/main/java/com/sqrtf/common/StringUtil.kt
@@ -2,6 +2,7 @@ package com.sqrtf.common
 
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by roya on 2017/7/20.
@@ -11,20 +12,22 @@ class StringUtil {
 
     companion object {
         private var dayFormatter = SimpleDateFormat("EE", Locale.getDefault())
-        private var msFormatter = SimpleDateFormat("mm:ss", Locale.getDefault())
         private val oneDay = 86400000
-
-        init {
-            msFormatter.timeZone = TimeZone.getTimeZone("UTC")
-        }
-
 
         fun dayOfWeek(day: Int): String {
             return dayFormatter.format(day * oneDay + 3 * oneDay)
         }
 
+        private fun addPadding(string: String): String {
+            return (if (string.length < 2) "0" else "") + string
+        }
+
         fun microsecondFormat(ms: Long): String {
-            return msFormatter.format(ms)
+            return addPadding("" + TimeUnit.MILLISECONDS.toMinutes(ms)) +
+                    ":" +
+                    addPadding("" + (TimeUnit.MILLISECONDS.toSeconds(ms) -
+                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(ms))))
+
         }
     }
 }
